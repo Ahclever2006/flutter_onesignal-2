7c8ce0de41096da1c898d6f92381c3d523e4dc54
Carlos Millan <atlantis9@hotmail.com>
2019-09-07 19:44:07 -0500

    Mi primer commit

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:onesignal/onesignal.dart';
+import 'package:onesignal_ejemplo/storage_screen.dart';
+
+void main() {
+  OneSignal.shared.init("34bf67d0-7d40-4778-b48a-629e3a974aa1");
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _openNewPage() {
+    //Navigator.of(context).pushNamed('/widget');
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => StorageScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+            SizedBox(height: 15),
+            RaisedButton(
+              child: Text('Ir a Images'),
+              onPressed: (){
+                _openNewPage();
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/storage_screen.dart
@@ -0,0 +1,58 @@
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+class StorageScreen extends StatefulWidget {
+  @override
+  _StorageScreenState createState() => _StorageScreenState();
+}
+
+class _StorageScreenState extends State<StorageScreen> {
+
+  List<dynamic> imagenes = [];
+  
+  @override
+  void initState() {
+    //TODO: todo
+    super.initState();
+    _obtenerImagenes();
+  }
+  // https://firebasestorage.googleapis.com/v0/b/fir-flutter-62ba6.appspot.com/o/imagenes%2Fimagen1.jpg?alt=media&token=9e071e42-6763-43b2-8c8e-06295e4d1d75
+  // Image.network('https://firebasestorage.googleapis.com/v0/b/fir-flutter-62ba6.appspot.com/o/${imagenes[index]['name']}?alt=media&token=9e071e42-6763-43b2-8c8e-06295e4d1d75');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('prueba'),
+      ),
+      body: Container(
+        child: GridView.builder(
+          itemCount: imagenes.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              git child: Image.network('https://firebasestorage.googleapis.com/v0/b/fir-flutter-62ba6.appspot.com/o/${imagenes[index]}?alt=media')
+            );
+          }
+        )
+      )
+    );
+  }
+
+  _obtenerImagenes() async {
+    String url = 'https://firebasestorage.googleapis.com/v0/b/fir-flutter-62ba6.appspot.com/o/';
+    var response = await http.get(url);
+    if(response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body)['items'];
+      for(int i=0; i < jsonResponse.length; i++) {
+        var temp = jsonResponse[i]['name'].replaceAll('/', '%2F');
+        imagenes.add(temp);
+      }
+      print(imagenes);
+    } else {
+      print('Erorrrrr');
+    }
+  }
+}
+
